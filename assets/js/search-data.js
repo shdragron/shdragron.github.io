c14b982c6bd0c7f062fc27c9cd1ea98950185a1c
shdragron <82024215+shdragron@users.noreply.github.com>
2026-05-09 13:48:55 +0000

    Deploying to gh-pages from @ shdragron/shdragron.github.io@e4b6e323a8c7157f80dd3699de70ecc413f6b154 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-floragonis",
-    title: "Floragonis",
+    id: "nav-shdragron",
+    title: "SHDragron",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
